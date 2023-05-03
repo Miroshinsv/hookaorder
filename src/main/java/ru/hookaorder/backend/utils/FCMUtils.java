114b22ac7d63a3bb1753c814cf315ad018840050
Miroshin Sergey <baloo@wisebits.com>
2023-05-03 19:09:54 +0300

    test dockerfile

--- src/main/java/ru/hookaorder/backend/utils/FCMUtils.java
+++ src/main/java/ru/hookaorder/backend/utils/FCMUtils.java
@@ -1,6 +1,7 @@
 package ru.hookaorder.backend.utils;
 
 import com.google.firebase.messaging.Message;
+import com.google.firebase.messaging.Notification;
 import ru.hookaorder.backend.feature.order.entity.OrderEntity;
 
 public final class FCMUtils {
@@ -14,6 +15,7 @@ public final class FCMUtils {
             .putData("phone", userPhone)
             .putData("time", orderEntity.getOrderTime())
             .putData("comment", orderEntity.getComment() != null ? orderEntity.getComment().getText() : "")
+            .setNotification(Notification.builder().setTitle("Новый заказ от " + userPhone).setBody("Комментарий: " + orderEntity.getComment().getText()).build())
             .build();
     }
 }
