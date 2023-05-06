42f9e24a27213975b20b101c7f6d5585b2271a5d
Miroshin Sergey <baloo@wisebits.com>
2023-05-06 23:23:12 +0300

    return phone

--- src/main/java/ru/hookaorder/backend/feature/user/entity/UserEntity.java
+++ src/main/java/ru/hookaorder/backend/feature/user/entity/UserEntity.java
@@ -35,7 +35,6 @@ public class UserEntity extends BaseEntity {
 
     @Column(name = "phone", nullable = false, unique = true)
     @Pattern(regexp = "^[+]?[(]?[0-9]{3}[)]?[-\\s.]?[0-9]{3}[-\\s.]?[0-9]{4,6}$")
-    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     private String phone;
 
     @NotBlank
