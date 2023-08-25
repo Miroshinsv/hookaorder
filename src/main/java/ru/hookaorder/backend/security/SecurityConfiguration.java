c35b5ccfd3fe859ecf0641841183f16a16216a19
Miroshin Sergey <baloo@wisebits.com>
2023-08-25 17:47:54 +0300

    disable cors

--- src/main/java/ru/hookaorder/backend/security/SecurityConfiguration.java
+++ src/main/java/ru/hookaorder/backend/security/SecurityConfiguration.java
@@ -6,7 +6,10 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
 import ru.hookaorder.backend.feature.JWT.service.JwtFilter;
+
+import java.util.List;
 
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
@@ -20,6 +23,13 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .csrf()
                 .and()
                 .cors()
+                .configurationSource(request -> {
+                    var cors = new CorsConfiguration();
+                    cors.setAllowedOrigins(List.of("http://127.0.0.1:80", "http://web.hookahorder.ru/","http://hookahorder.ru/"));
+                    cors.setAllowedMethods(List.of("GET","POST", "PUT", "DELETE", "OPTIONS"));
+                    cors.setAllowedHeaders(List.of("*"));
+                    return cors;
+                })
                 .disable()
                 .authorizeHttpRequests()
                 .antMatchers("/auth/login", "/place/**", "/user/create").permitAll()
